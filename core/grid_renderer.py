5036778de90ba8e39b9921aa0e8cd71875699332
25nm774s@vc.ibaraki.ac.jp <21t7039t@vc.ibaraki.ac.jp>
2025-06-20 11:05:26 +0900

    feat: レンダリング処理を GridRenderer クラスに分離
    GridWorld クラスから描画に関する責務を分離し、
    新たに GridRenderer クラスを作成しました。
    
    - GridWorld クラスから描画関連のメソッドとインスタンス変数を削除。
    - GridWorld クラスに GridRenderer のインスタンスを持たせ、描画処理を委譲。
    - GridRenderer クラスに描画に必要なロジックを実装。
    
    これにより、GridWorld クラスは環境のロジックに集中し、
    コードの見通しと保守性が向上しました。

---
+++ core/grid_renderer.py
@@ -0,0 +1,87 @@
+"""
+グリッドワールドのレンダリングを行うクラス．
+pygameを用いて環境を描画する．
+"""
+
+import pygame
+
+# カラー定義
+BLACK  = (0, 0, 0)
+WHITE  = (255, 255, 255)
+GREEN  = (0, 255, 0)
+BLUE   = (0, 0, 255)
+GRAY   = (128, 128, 128)
+
+class GridRenderer:
+    def __init__(self, window_width, window_height, cell_num):
+        """コンストラクタ：ウィンドウサイズや環境パラメータの初期設定"""
+        self.window_width = window_width
+        self.window_height = window_height
+        self.cell_num = cell_num
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.window_width, self.window_height))
+        self.font = pygame.font.SysFont(None, 36)
+
+    def render(self, goals, agents, episode_num=0, time_step=0):
+        """pygame を用いて環境を描画"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+
+        self._draw_grid()
+        self._draw_goals_and_agents(goals, agents)
+        self._draw_episode_and_step_info(episode_num, time_step)
+
+        pygame.display.flip()
+
+    def _draw_grid(self):
+        """マス目を描画"""
+        cell_w = self.window_width / self.cell_num
+        cell_h = self.window_height / self.cell_num
+        self.screen.fill(GRAY)
+
+        for x in range(self.cell_num):
+            for y in range(self.cell_num):
+                rect = pygame.Rect(x * cell_w, y * cell_h, cell_w, cell_h)
+                pygame.draw.rect(self.screen, BLACK, rect, 1)
+
+    def _draw_goals_and_agents(self, goals, agents):
+        """ゴールとエージェントを描画"""
+        cell_w = self.window_width / self.cell_num
+        cell_h = self.window_height / self.cell_num
+
+        # ゴールの描画
+        for goal in goals:
+            rect = pygame.Rect(goal[0] * cell_w, goal[1] * cell_h, cell_w, cell_h)
+            pygame.draw.rect(self.screen, GREEN, rect)
+
+        # エージェントの描画
+        agent_w = cell_w * 0.8
+        agent_h = cell_h * 0.8
+        font = pygame.font.Font(None, int(cell_h * 0.5))
+
+        for idx, agent in enumerate(agents):
+            rect = pygame.Rect(
+                agent[0] * cell_w + (cell_w - agent_w) / 2,
+                agent[1] * cell_h + (cell_h - agent_h) / 2,
+                agent_w,
+                agent_h
+            )
+            pygame.draw.rect(self.screen, BLUE, rect)
+
+            # エージェント番号
+            text_surf = font.render(str(idx), True, WHITE)
+            text_rect = text_surf.get_rect(center=(
+                agent[0] * cell_w + cell_w / 2,
+                agent[1] * cell_h + cell_h / 2
+            ))
+            self.screen.blit(text_surf, text_rect)
+
+    def _draw_episode_and_step_info(self, episode_num, time_step):
+        """エピソード数とステップ数を画面に描画"""
+        ep_text = self.font.render(f'Episode: {episode_num}', True, BLACK)
+        st_text = self.font.render(f'Step: {time_step}', True, BLACK)
+        self.screen.blit(ep_text, (10, 10))
+        self.screen.blit(st_text, (10, 50))

2c8e0bf8de0b4147e7c098c68742d665de40ee06
25nm774s@vc.ibaraki.ac.jp <21t7039t@vc.ibaraki.ac.jp>
2025-12-21 13:50:01 +0900

    feat: カスタムタイプ宣言

---
+++ src/Base/Constant.py
@@ -0,0 +1,23 @@
+from typing import Tuple,List,Any,Dict
+
+# 環境から受け取る座標はこっちを使用　共通の座標表現形式
+PosType = Tuple[int,int]
+"""共通の座標表現形式"""
+
+GlobalState = Tuple[PosType,...]
+"""共通の状態表現s"""
+
+#####################################
+# Qテーブル内部の座標表現形式
+QState = Tuple[int, ...]
+"""Qテーブル入力用の座標表現形式(旧QState:)"""
+
+QValues = List[float]
+"""行動に対するQ値"""
+
+QTableType = Dict[QState, QValues]
+"""Qテーブルの構造"""
+
+####################################
+# DQN内部の座標表現形式
+# 未定義
